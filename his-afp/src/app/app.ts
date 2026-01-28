e6748b1e7c506c698dbd89b1223f92f27fc02739
Pietro Rocchio <pietro.rocchio@gmail.com>
2026-01-28 10:40:47 +0100

    chore: creato componente card-pz

--- his-afp/src/app/app.ts
+++ his-afp/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { DarkmodeSelector } from './darckmode-selector/darkmode-selector.component';
+import { DarkmodeSelector } from './darkmode-selector/darkmode-selector.component';
+import { CardPz } from './card-pz/card-pz';
 
 @Component({
   selector: 'app-root',
-  imports: [DarkmodeSelector],
+  imports: [DarkmodeSelector, DarkmodeSelector, CardPz],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
---
+++ his-afp/src/app/card-pz/card-pz.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+
+@Component({
+  selector: 'his-card-pz',
+  imports: [CardModule],
+  templateUrl: './card-pz.html',
+  styleUrl: './card-pz.scss',
+})
+export class CardPz {}
