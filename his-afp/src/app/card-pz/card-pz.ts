3c39bc6e31f2719b09bf5109fd78bcff731d1bea
matteo-bertotti <matteo.bertotti@mat.tn.it>
2026-02-05 12:22:47 +0100

    feat: ✨ Refactor CardPz component to enhance patient display and state color logic

--- his-afp/src/app/card-pz/card-pz.ts
+++ his-afp/src/app/card-pz/card-pz.ts
@@ -1,9 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, input} from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { Button } from 'primeng/button';
 import { JsonPipe } from '@angular/common';
 
-interface Paziente {
+export interface Paziente {
   id: string;
   nome: string;
   cognome: string;
@@ -15,28 +15,40 @@ interface Paziente {
 }
 
 @Component({
-  selector: 'his-card-pz',
+  selector: 'app-card-pz',
   imports: [CardModule, Button, JsonPipe],
   templateUrl: './card-pz.html',
   styleUrl: './card-pz.scss',
 })
 export class CardPz {
-  nome: string = 'Pietro';
-  paziente = signal<Paziente>({
-    braccialetto: '123',
-    codiceColore: 'ROSSO',
-    cognome: 'Rocchio',
-    eta: 25,
-    id: '23',
-    nome: 'Pietro',
-    note: 'TRauma',
-    patologia: 'C19',
-  });
+  paziente = input.required<Paziente>();
+  // Uso local interno componente
+  // paziente = signal<Paziente>({
+  //   braccialetto: '123',
+  //   codiceColore: 'ROSSO',
+  //   cognome: 'Rocchio',
+  //   eta: 25,
+  //   id: '23',
+  //   nome: 'Pietro',
+  //   note: 'TRauma',
+  //   patologia: 'C19',
+  // });
 
-  cambiaNome() {
-    this.nome = 'Gian';
-
-    // this.paziente = "Lucio";
-    //this.paziente.set('Lucio');
+  setColoreDiStato() {
+    switch (this.paziente().codiceColore) {
+      case 'ROSSO':
+        return 'border-red-600';
+      case 'ARANCIONE':
+        return 'border-yellow-600';
+      case 'AZZURRO':
+        return 'border-blue-600';
+      case 'VERDE':
+        return 'border-green-600';
+      case 'BIANCO':
+        return 'border-white-600';
+      default:
+        return '';
+    }
   }
 }
+
