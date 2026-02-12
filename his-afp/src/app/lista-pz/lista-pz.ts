d109487a685142ec1d2f8541299cde1c49d7ab83
matteo-bertotti <matteo.bertotti@mat.tn.it>
2026-02-12 12:24:38 +0100

    feat: ✨ Enhance ListaPz component with health status retrieval and filtering functionality

--- his-afp/src/app/lista-pz/lista-pz.ts
+++ his-afp/src/app/lista-pz/lista-pz.ts
@@ -1,13 +1,30 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, model, computed, inject } from '@angular/core';
 import { CardPz, Paziente } from '../card-pz/card-pz';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { TagModule } from 'primeng/tag';
+
+interface Response {
+  status: string;
+  data: HealthStatus;
+};
+interface HealthStatus {
+  service: string;
+  database: string;
+  uptime: number;
+};
 
 @Component({
   selector: 'app-lista-pz',
-  imports: [CardPz],
+  imports: [CardPz, InputTextModule, FormsModule, TagModule],
   templateUrl: './lista-pz.html',
   styleUrl: './lista-pz.scss',
 })
 export class ListaPz {
+  readonly #http = inject(HttpClient);
+
+  nomePaziente = model<string>('');
   listaPz = signal<Paziente[]>([
     {
       braccialetto: '1',
@@ -30,4 +47,21 @@ export class ListaPz {
       patologia: 'C69',
     },
   ]);
+  healthStatus = signal<HealthStatus | null>(null);
+  filteredList = computed(() => {
+    return this.listaPz().filter((pz) =>
+      pz.nome.toLowerCase().includes(this.nomePaziente().toLowerCase()),
+    );
+  });
+
+  getHealthStatus(){
+    this.#http.get<Response>('http://localhost:3000/health').subscribe((res) => {
+      console.log(res);
+      this.healthStatus.set(res.data);
+    });
+  };
+
+  constructor() {
+    this.getHealthStatus();
+  }
 }
