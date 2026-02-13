e47916255e41bb2d26de1f0f2bf99c8cfe7ad520
Pietro Rocchio <pietro.rocchio@gmail.com>
2026-02-13 14:42:02 +0100

    feat: ✨ enhance health status retrieval with error handling
    Improve the health status retrieval functionality by adding error handling for database connectivity. The response now indicates whether the database is connected or disconnected based on the query result. In case of an error, a 503 status is returned with an appropriate error message, ensuring better reliability and clarity in service status reporting.

--- backend/services/health.js
+++ backend/services/health.js
@@ -1,14 +1,28 @@
-import { pool } from "./dbSession.js";
-import { catchAsync } from "../utils/errorHandler.js";
+import {pool} from "./dbSession.js";
+import {catchAsync} from "../utils/errorHandler.js";
 
 export const retrieveHealthStatusFn = catchAsync(async (req, res) => {
-	await pool.query('SELECT 1');
-	res.status(200).json({
-		status: 'success',
-		data: {
-			service: 'UP',
-			database: 'CONNECTED',
-			uptime: process.uptime()
-		}
-	});
+	try {
+		const dbStatus = await pool.query('SELECT 1');
+		res.status(200).json({
+			status: 'success',
+			data: {
+				service: 'UP',
+				database: dbStatus.rowCount === 1 ? 'CONNECTED' : 'DISCONNECTED',
+				uptime: process.uptime()
+			}
+		});
+	} catch (err) {
+		res.status(503).json({
+			status: 'error',
+			data: {
+				service: 'UP',
+				database: 'DISCONNECTED',
+				uptime: process.uptime()
+			},
+			message: err.message
+		});
+	}
+
+
 });
