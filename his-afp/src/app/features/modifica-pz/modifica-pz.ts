fa31580345ddb529191d195a02a423eae218dd0a
matteo-bertotti <matteo.bertotti@mat.tn.it>
2026-03-04 15:49:01 +0100

    feat: ✨ add ModificaPz component with template, styles, and tests

---
+++ his-afp/src/app/features/modifica-pz/modifica-pz.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'his-modifica-pz',
+  imports: [],
+  templateUrl: './modifica-pz.html',
+  styleUrl: './modifica-pz.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ModificaPz {
+
+}
