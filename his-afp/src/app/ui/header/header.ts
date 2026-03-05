3615899de5b5500172cb27d468ea07a609bf6f95
matteo-bertotti <matteo.bertotti@mat.tn.it>
2026-03-05 12:27:39 +0100

    feat: ✨ add Header component with template, styles, and tests

---
+++ his-afp/src/app/ui/header/header.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Button } from "primeng/button";
+import { Divider } from 'primeng/divider';
+import { DarkmodeSelector } from "../darkmode-selector/darkmode-selector.component";
+
+@Component({
+  selector: 'his-header',
+  imports: [Button, RouterLink, DarkmodeSelector, Divider],
+  templateUrl: './header.html',
+  styleUrl: './header.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Header {
+
+}
