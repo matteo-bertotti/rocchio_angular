d6e266b8d0370d71fbdcdc208a3532694ddab735
matteo-bertotti <matteo.bertotti@mat.tn.it>
2026-03-11 14:58:00 +0100

    feat: :sparkles: implemented stato-api component
    Implemented stato-api compoennet in stato-servizi component reacable from button nav

--- his-afp/src/app/features/stato-servizi/stato-servizi.ts
+++ his-afp/src/app/features/stato-servizi/stato-servizi.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { StatoAPI } from "../../ui/stato-api/stato-api";
 
 @Component({
   selector: 'his-stato-servizi',
-  imports: [],
+  imports: [StatoAPI],
   templateUrl: './stato-servizi.html',
   styleUrl: './stato-servizi.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
