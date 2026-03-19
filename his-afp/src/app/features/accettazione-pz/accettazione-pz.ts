fd71e5f2475456a7c68f4b8dba231d82f1751c04
matteo-bertotti <matteo.bertotti@mat.tn.it>
2026-03-19 12:07:31 +0100

    feat: ✨ enhance accettazione-pz component with form validation and error handling for nome input

--- his-afp/src/app/features/accettazione-pz/accettazione-pz.ts
+++ his-afp/src/app/features/accettazione-pz/accettazione-pz.ts
@@ -1,15 +1,18 @@
 
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { GestioneRisorse } from '../../core/Risorse/gestione-risorse';
+import { InputText } from "primeng/inputtext";
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'his-accettazione-pz',
-  imports: [ JsonPipe],
+  imports: [ InputText, ReactiveFormsModule, JsonPipe],
   templateUrl: './accettazione-pz.html',
   styleUrl: './accettazione-pz.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AccettazionePz {
   gestioneRisorse = inject(GestioneRisorse);
+  nome = new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(30)]);
 }
