923c585c80a647af39783949ceee682e342a7e2c
matteo-bertotti <matteo.bertotti@mat.tn.it>
2026-03-19 09:51:25 +0100

    feat: ✨ implement GestioneRisorse service with data fetching methods for triage colors, pathologies, and arrival modes

---
+++ his-afp/src/app/core/Risorse/gestione-risorse.spec.ts
@@ -0,0 +1,17 @@
+
+import { TestBed } from '@angular/core/testing';
+
+import { GestioneRisorse } from './gestione-risorse';
+
+describe('GestioneRisorse', () => {
+  let service: GestioneRisorse;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(GestioneRisorse);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
