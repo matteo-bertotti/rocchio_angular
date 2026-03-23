49855e7c957d1ad4ef100986e5935166959b00f8
matteo-bertotti <matteo.bertotti@mat.tn.it>
2026-03-23 13:04:35 +0100

    feat: ✨ implement reactive form for accettazione-pz component with validation for nome and cognome fields

--- his-afp/src/app/features/accettazione-pz/accettazione-pz.ts
+++ his-afp/src/app/features/accettazione-pz/accettazione-pz.ts
@@ -1,18 +1,62 @@
-
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { GestioneRisorse } from '../../core/Risorse/gestione-risorse';
-import { InputText } from "primeng/inputtext";
-import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
-import { JsonPipe } from '@angular/common';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {GestioneRisorse} from '../../core/Risorse/gestione-risorse';
+import {InputText} from 'primeng/inputtext';
+import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {JsonPipe} from '@angular/common';
+import {Button} from 'primeng/button';
 
 @Component({
   selector: 'his-accettazione-pz',
-  imports: [ InputText, ReactiveFormsModule, JsonPipe],
+  imports: [InputText, ReactiveFormsModule, JsonPipe, Button],
   templateUrl: './accettazione-pz.html',
   styleUrl: './accettazione-pz.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AccettazionePz {
-  gestioneRisorse = inject(GestioneRisorse);
-  nome = new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(30)]);
+  //gestioneRisorse = inject(GestioneRisorse);
+
+  // paziente = new FormGroup({
+  //   nome: new FormControl('', [Validators.required]),
+  //   cognome: new FormControl('', [Validators.required]),
+  // });
+  readonly #fb = inject(FormBuilder);
+  paziente = this.#fb.group({
+    anagrafica: this.#fb.group({
+        nome: ['', [Validators.required]],
+        cognome: ['', [Validators.required]],
+        datanascita: ['', [Validators.required]],
+        codiceFiscale: ['', [Validators.required]],
+        sesso: ['', [Validators.required]],
+    }),
+    sanitaria: this.#fb.group({
+      patologia: ['', [Validators.required]],
+      codiceColore: ['', [Validators.required]],
+      modArrivo: ['', [Validators.required]],
+      noteTriage: ['', [Validators.required]],
+    }),
+  });
+
+  checkFormControl(control: string) {
+    const fc = this.paziente.get(control);
+    // nome.invalid && (nome.touched || nome.dirty)
+    return fc?.invalid && (fc.touched || fc.dirty);
+  }
+
+  checkFormControlError(control: string, err: string) {
+    const fc = this.paziente.get(control);
+
+    if (fc && fc.hasError(err)) {
+      return fc.getError(err);
+    } else {
+      return null;
+    }
+  }
+
+  onSubmit() {
+    if (this.paziente.valid) {
+      console.log(this.paziente.value);
+    } else {
+      this.paziente.markAllAsTouched();
+    }
+  }
 }
